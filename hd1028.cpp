ac83f408b74940e5763ef517281d14eb4fdc5a8f
nwpuhh <huhao_insa@icloud.com>
2018-01-05 22:12:41 +0100

    The hd1028 is not finished, there are some error

---
+++ hd1028.cpp
@@ -0,0 +1,18 @@
+//应当使用递推，网上介绍的方法是使用多项式的方式，自我寻找中
+#include <iostream>
+using namespace std;
+
+int get_num_equ(int num)
+{
+
+}
+
+int main()
+{
+    int N;
+    while(cin >> N)
+    {
+        cout << get_num_equ(N) << endl;
+    }
+    return 0;
+}
